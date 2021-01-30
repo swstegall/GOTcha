f83ac87560ff8a9ea1dfb72c09df3204512d6310
Samuel Wallace Stegall <stegall@tamu.edu>
2021-01-30 14:51:58 -0600

    Created a nice footer.

--- GOTchaApp/App.js
+++ GOTchaApp/App.js
@@ -11,17 +11,48 @@ import SocialScreen from "./screens/SocialScreen";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const characterImage = "😎";
+  const timeRemaining = "4:00";
+  const currentStamina = "0";
+  const totalStamina = "4";
+  const footerParams = {
+    characterImage,
+    timeRemaining,
+    currentStamina,
+    totalStamina,
+  };
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Drawer.Navigator initialRouteName={"Home"}>
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Battle" component={BattleScreen} />
-          <Drawer.Screen name="Scan" component={ScanScreen} />
-          <Drawer.Screen name="Purchase" component={PurchaseScreen} />
-          <Drawer.Screen name="Social" component={SocialScreen} />
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            initialParams={footerParams}
+          />
+          <Drawer.Screen
+            name="Battle"
+            component={BattleScreen}
+            initialParams={footerParams}
+          />
+          <Drawer.Screen
+            name="Scan"
+            component={ScanScreen}
+            initialParams={footerParams}
+          />
+          <Drawer.Screen
+            name="Purchase"
+            component={PurchaseScreen}
+            initialParams={footerParams}
+          />
+          <Drawer.Screen
+            name="Social"
+            component={SocialScreen}
+            initialParams={footerParams}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
-};
+}
--- GOTchaApp/screens/HomeScreen.jsx
+++ GOTchaApp/screens/HomeScreen.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { CustomHeader as Header } from "../components/CustomHeader";
+import { CustomFooter as Footer } from "../components/CustomFooter";
+import ScreenStyle from "../styles/ScreenStyle";
 
 const HomeScreen = (props) => {
+  const footerOptions = props.route.params;
   return (
-    <View>
+    <View style={ScreenStyle.container}>
       <Header navigation={props.navigation} title={"HOME"} />
       <Text>HomeScreen</Text>
+      <Footer footerOptions={footerOptions} />
     </View>
   );
 };
