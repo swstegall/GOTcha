ef78d681e87053dff762faa9ff469a94e82095b2
Karli Kessler <karli.kessler@gmail.com>
2021-01-30 15:38:09 -0600

    Basic login screen.

--- GOTchaApp/screens/LoginScreen.jsx
+++ GOTchaApp/screens/LoginScreen.jsx
@@ -4,17 +4,17 @@ import { Alert, Button, TextInput, View, StyleSheet } from "react-native";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     };
   }
-  
+
   onLogin() {
     const { username, password } = this.state;
 
-    Alert.alert('Credentials', `${username} + ${password}`);
+    Alert.alert("Credentials", `${username} + ${password}`);
   }
 
   render() {
@@ -23,19 +23,19 @@ export default class App extends Component {
         <TextInput
           value={this.state.username}
           onChangeText={(username) => this.setState({ username })}
-          placeholder={'Username'}
+          placeholder={"Username"}
           style={styles.input}
         />
         <TextInput
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
-          placeholder={'Password'}
+          placeholder={"Password"}
           secureTextEntry={true}
           style={styles.input}
         />
-        
+
         <Button
-          title={'Login'}
+          title={"Login"}
           style={styles.input}
           onPress={this.onLogin.bind(this)}
         />
@@ -47,16 +47,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
   },
   input: {
     width: 200,
     height: 44,
     padding: 10,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     marginBottom: 10,
   },
 });
